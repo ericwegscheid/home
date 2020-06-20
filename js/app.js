d2878ee18fe8dd4dbf9de0f63c5b2650393cbf0e
Eric Wegscheid <ericwegscheid@gmail.com>
2020-06-20 11:30:25 -0500

    -

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@
 
   window._app = {
     init: () => {
-      console.log(
-        __.select('.wrapper')
-      );
-      console.log(
-        __.select('.wrapper', true)
-      );
+      console.log(__.select('.wrapper'));
+      console.log(__.select('.wrapper', true));
+      __.EVENTS.enableDelegationEvents('fnclick', 'fnenter', _app.handlers);
+    },
+    handlers: {
+      test: e => {
+        console.log('test');
+      },
+      otherTest: e => {
+        console.log('other test');
+      }
     }
   };
 
